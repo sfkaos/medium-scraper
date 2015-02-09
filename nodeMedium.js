745481afd23240a0dd603977fe1202306c5429bb
Harris Robin Kalash <sirrah@Harriss-MacBook.local>
2015-02-09 02:11:52 -0500

    latest

---
+++ nodeMedium.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var cors = require("cors");
+var medium = require('node-medium');
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/rvmedium');
+
+var PostSchema = new Schema({
+  name: String
+});
+
+var Post = mongoose.model('Post', PostSchema);
+var app = express();
+app.use(cors());
+
+
+
+app.get('/', function(req,res) {
+
+  // https://medium.com/@yelpmontreal
+  medium.getUser('yelpmontreal', function(data) {
+    var postArr = data.posts;
+    postArr.forEach(function(post) {
+      return medium.getPost(null, post.id, function(data) {
+        console.log(data);
+        res.json(data);
+      });
+    });
+  });
+
+});
+
+
+app.listen(3000);
