6f86b4cea5b9607a0eb8912e1e849ec8764abfa0
Harris Robin Kalash <sirrah@Harriss-MacBook.local>
2015-02-09 10:21:41 -0500

    latest

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ var express = require('express');
 var cors = require("cors");
 var medium = require('node-medium');
 
-var Post = mongoose.model('Post', PostSchema);
 var app = express();
 app.use(cors());
 
 
 
 app.get('/', function(req,res) {
-
   // https://medium.com/@yelpmontreal
   medium.getUser('yelpmontreal', function(data) {
     var postArr = data.posts;
     postArr.forEach(function(post) {
-      medium.getPost(null, post.id, function(data) {
-        console.log(data);
+     medium.getPost(null, post.id, function(data) {
+        res.write(JSON.stringify(data));
       });
-      res.json(data);
     });
+    res.end();
   });
 
 });
