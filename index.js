e079eb66efe4e4c5ca660d1882f07f1d379f1bdc
Harris Robin Kalash <harrisrobinkalash@gmail.com>
2015-05-02 20:12:07 -0400

    began writing boilerplate code to scrape images

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require("cors");
 var medium = require('node-medium');
+var request = require('request');
+var cheerio = require('cheerio');
 
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
@@ -24,14 +26,15 @@ app.use(cors());
 
 app.get('/', function(req,res) {
   // https://medium.com/@elleluna
-  medium.getUser('harrisrobinkalash', function(data) {
-    //console.log(data);
+  medium.getUser('elleluna', function(data) {
+    //console.log('user', data);
     var postArr = data.posts;
     postArr.forEach(function(post) {
+      //console.log(post);
      medium.getPost(null, post.id, function(data) {
        // console.log('data', data);
         mediumPost.update({title: data.title}, data, {upsert: true}, function(err) {
-          //console.log('successfully saved', data.paragraphs[10]);
+          console.log('successfully saved', data);
         });
       });
     });
@@ -44,6 +47,43 @@ app.get('/', function(req,res) {
 });
 
 
+app.get('/scrape', function(req,res) {
+
+
+    var url = 'https://medium.com/@elleluna';
+
+    request(url, function(error, response, html)  {
+      // First we'll check to make sure no errors occurred when making the request
+      if(!error)  {
+        // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+        var $ = cheerio.load(html);
+
+        $('.blockGroup--latest.blockGroup--posts.blockGroup--list .block.block--list').filter(function() {
+          var data = $(this);
+          // console.log('data', data);
+          data.each(function(index, el) {
+            var postURL = $(this).find('a.block-linkHack').attr('href');
+
+
+            request(postURL, function(error, response, html)  {
+              var $$ = cheerio.load(html);
+              console.log('body of posts', $$('.postField.postField--body img.graf-image').attr('src'));
+            });
+
+
+          });
+        });
+      }
+    });
+
+
+      return res.json(200, {});
+
+});
+
+
+
+
 app.get('/:id', function(req,res) {
   return mediumPost.findById(req.params.id, function (err, post) {
     if (!err) {
@@ -53,6 +93,7 @@ app.get('/:id', function(req,res) {
     }
   });
 });
+
 
 
 app.listen(process.env.PORT || 3000);
