43e09976b5a087cb8281951680898473559d4607
Harris Robin Kalash <harrisrobinkalash@gmail.com>
2015-06-11 21:05:41 -0400

    updated api to only return urls with their respective images

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var mediumPostSchema = new Schema({
-  title: String,
-  createdAt: { type: Date, default: Date.now },
-  subTitle: String,
-  author: {},
-  paragraphs: []
-
+  //title: String,
+  // createdAt: { type: Date, default: Date.now },
+  // subTitle: String,
+  // author: {},
+  // paragraphs: [],
+  url: String,
+  html: String,
+  image: String
 });
 
 var mediumPost = mongoose.model('mediumPost', mediumPostSchema);
@@ -24,21 +26,67 @@ app.use(cors());
 
 
 
+// app.get('/', function(req,res) {
+//   // https://medium.com/@elleluna
+//   medium.getUser('elleluna', function(data) {
+//     //console.log('user', data);
+//     var postArr = data.posts;
+//     postArr.forEach(function(post) {
+//       //console.log(post);
+//      medium.getPost(null, post.id, function(data) {
+//        // console.log('data', data);
+//         mediumPost.update({title: data.title}, data, {upsert: true}, function(err) {
+//           console.log('successfully saved', data);
+//         });
+//       });
+//     });
+//   });
+
+//   mediumPost.find(function (err, posts) {
+//     return res.json(200, posts);
+//   });
+
+// });
+
 app.get('/', function(req,res) {
-  // https://medium.com/@elleluna
-  medium.getUser('elleluna', function(data) {
-    //console.log('user', data);
-    var postArr = data.posts;
-    postArr.forEach(function(post) {
-      //console.log(post);
-     medium.getPost(null, post.id, function(data) {
-       // console.log('data', data);
-        mediumPost.update({title: data.title}, data, {upsert: true}, function(err) {
-          console.log('successfully saved', data);
+
+    var url = 'https://medium.com/@elleluna';
+
+    request(url, function(error, response, html)  {
+      // First we'll check to make sure no errors occurred when making the request
+        // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+      var $ = cheerio.load(html);
+
+      $('.blockGroup--latest.blockGroup--posts.blockGroup--list .block.block--list').filter(function() {
+        var data = $(this);
+
+        //console.log('datum',$(this));
+
+        data.each(function(index, el) {
+          var postURL = $(this).find('.postArticle a').attr('href');
+          console.log('postURL', postURL);
+
+          request(postURL, function(error, response, html)  {
+            var $$ = cheerio.load(html);
+            var grafImage = $$('.graf-image').attr('src');
+            //console.log('dolla dolla',$$('.postArticle-content'));
+            //console.log('body of posts', $$('.site-main').html());
+
+            var model = {
+              url: postURL,
+              image: grafImage
+            }
+
+
+            mediumPost.update({url: postURL}, model, {upsert: true}, function(err) {
+              //console.log('successfully saved', $$);
+            });
+
+          });
+
         });
       });
     });
-  });
 
   mediumPost.find(function (err, posts) {
     return res.json(200, posts);
@@ -47,53 +95,17 @@ app.get('/', function(req,res) {
 });
 
 
-app.get('/scrape', function(req,res) {
 
-
-    var url = 'https://medium.com/@elleluna';
-
-    request(url, function(error, response, html)  {
-      // First we'll check to make sure no errors occurred when making the request
-      if(!error)  {
-        // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-        var $ = cheerio.load(html);
-
-        $('.blockGroup--latest.blockGroup--posts.blockGroup--list .block.block--list').filter(function() {
-          var data = $(this);
-          // console.log('data', data);
-          data.each(function(index, el) {
-            var postURL = $(this).find('a.block-linkHack').attr('href');
-
-
-            request(postURL, function(error, response, html)  {
-              var $$ = cheerio.load(html);
-              console.log('body of posts', $$('.postField.postField--body img.graf-image').attr('src'));
-            });
-
-
-          });
-        });
-      }
-    });
-
-
-      return res.json(200, {});
-
-});
+// app.get('/:id', function(req,res) {
+//   return mediumPost.findById(req.params.id, function (err, post) {
+//     if (!err) {
+//       return res.json(200, post);
+//     } else {
+//       return console.log(err);
+//     }
+//   });
+// });
 
 
 
-
-app.get('/:id', function(req,res) {
-  return mediumPost.findById(req.params.id, function (err, post) {
-    if (!err) {
-      return res.json(200, post);
-    } else {
-      return console.log(err);
-    }
-  });
-});
-
-
-
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
