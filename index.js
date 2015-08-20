0901192f151a6e69f01f4dd7499ae5e7013b72e5
Harris Robin Kalash <harrisrobinkalash@gmail.com>
2015-08-20 01:18:51 -0400

    fixed date by giving it date.now

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var cors = require("cors");
+var cors    = require("cors");
 var request = require('request');
 var cheerio = require('cheerio');
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+    Schema   = mongoose.Schema;
 
 var mediumPostSchema = new Schema({
   title: String,
@@ -15,7 +15,7 @@ var mediumPostSchema = new Schema({
   url: String,
   html: String,
   image: String,
-  date: { type: Date, default: Date.now },
+  date: {type: Date, default: Date.now},
   description: String,
   tag: String
 
@@ -26,7 +26,6 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/RVmediumPosts'
 
 var app = express();
 app.use(cors());
-
 
 
 // app.get('/', function(req,res) {
@@ -51,94 +50,98 @@ app.use(cors());
 
 // });
 
-app.get('/', function(req,res) {
+app.get('/', function (req, res) {
 
-    var url = 'https://medium.com/@realventures';
+  var url = 'https://medium.com/@realventures';
 
-    function isNumeric(n) {
-      return !isNaN(parseFloat(n)) && isFinite(n);
-    }
+  function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
 
-    request(url, function(error, response, html)  {
-      // First we'll check to make sure no errors occurred when making the request
-        // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-      var $ = cheerio.load(html);
+  request(url, function (error, response, html) {
+    // First we'll check to make sure no errors occurred when making the request
+    // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+    var $ = cheerio.load(html);
 
-      $('.blockGroup--latest.blockGroup--posts.blockGroup--list .block.block--list').filter(function() {
-        var data = $(this);
-
-
-        data.each(function(index, el) {
-          var postURL = $(this).find('.postArticle a').attr('href');
-          var title = $(this).find('h2#title').text();
-          var description = $(this).find('h4#subtitle').text() || $(this).find('p.graf--p').text();
-          var d = $(this).find('.postMetaInline-feedSummary .postMetaInline--supplemental a.link').text();
-          var date = Date.parse(d);
+    $('.blockGroup--latest.blockGroup--posts.blockGroup--list .block.block--list').filter(function () {
+      var data = $(this);
 
 
-          //grab postID from url by matching it with a regex
-          var isolateID = postURL.substr(postURL.lastIndexOf('-') + 1);
-          var postID = isolateID.substring(0,12);
+      data.each(function (index, el) {
+        var postURL     = $(this).find('.postArticle a').attr('href');
+        var title       = $(this).find('h2#title').text();
+        var description = $(this).find('h4#subtitle').text() || $(this).find('p.graf--p').text();
+        var d           = $(this).find('.postMetaInline-feedSummary .postMetaInline--supplemental a.link').text();
+        var date;
+        if(isNumeric(date)) {
+          date = Date.parse(d);
+        } else {
+          date = Date.now();
+        }
 
-          var mediumTagsURL = 'https://medium.com/_/api/posts/' + postID + '/tags';
 
-          request(mediumTagsURL, function(error, response, html) {
-            //console.log('tags? is that you?', html);
-            var trimmed = html.substring(50);
-            var trimmed2 = trimmed.split(',')[0];
-            var plucked = trimmed2.split('"')[3];
+        //grab postID from url by matching it with a regex
+        var isolateID = postURL.substr(postURL.lastIndexOf('-') + 1);
+        var postID    = isolateID.substring(0, 12);
 
-            plucked === undefined ? plucked = 'uncategorized' : null;
+        var mediumTagsURL = 'https://medium.com/_/api/posts/' + postID + '/tags';
 
+        request(mediumTagsURL, function (error, response, html) {
+          //console.log('tags? is that you?', html);
+          var trimmed  = html.substring(50);
+          var trimmed2 = trimmed.split(',')[0];
+          var plucked  = trimmed2.split('"')[3];
+
+          plucked === undefined ? plucked = 'uncategorized' : null;
+
+          var model = {
+            title: title,
+            url: postURL,
+            //image: grafImage,
+            date: date,
+            description: description,
+            tag: plucked
+          }
+
+          mediumPost.update({}, model, {upsert: true},  function(err) {
+            console.log('successfully saved', model);
+          });
+
+        });
+
+
+        if(isNumeric(date)) {
+          request(postURL, function(error, response, html)  {
+            var $$ = cheerio.load(html);
+            var grafImage = $$('.graf-image').attr('src');
+
+            console.log('model', model);
             var model = {
               title: title,
               url: postURL,
-              //image: grafImage,
+              image: grafImage,
               date: date,
-              description: description,
-              tag: plucked
+              description: description
+              //tag: plucked
             }
 
-            mediumPost.update({date: date}, model, {upsert: true},  function(err) {
-              console.log('successfully saved', model);
+            mediumPost.update({}, model, {upsert: true},  function(err) {
+              console.log('successfully saved date numeric', model);
             });
+
 
           });
+        }
 
-
-          if(isNumeric(date)) {
-            request(postURL, function(error, response, html)  {
-              var $$ = cheerio.load(html);
-              var grafImage = $$('.graf-image').attr('src');
-
-              console.log('model', model);
-              var model = {
-                title: title,
-                url: postURL,
-                image: grafImage,
-                date: date,
-                description: description
-                //tag: plucked
-              }
-
-              mediumPost.update({title: title}, model, {upsert: true},  function(err) {
-                console.log('successfully saved', model);
-              });
-
-
-            });
-          }
-
-        });
       });
     });
+  });
 
   mediumPost.find(function (err, posts) {
     return res.json(200, posts);
   });
 
 });
-
 
 
 // app.get('/:id', function(req,res) {
@@ -150,7 +153,6 @@ app.get('/', function(req,res) {
 //     }
 //   });
 // });
-
 
 
 app.listen(process.env.PORT || 8080);
